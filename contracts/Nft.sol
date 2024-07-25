400b25dcd033da2f2c350b2425a27a754f1608e6
nuaing11 aja <poconuaing@gmail.com>
2024-07-25 07:56:16 +0000

    update

--- contracts/Nft.sol
+++ contracts/Nft.sol
@@ -8,7 +8,7 @@ contract TestNFT is ERC721 {
 
     event NFTMinted(address recipient, uint256 tokenId);
 
-    constructor() ERC721("ezieNFT", "ezieNFT") {}
+    constructor() ERC721("Arieza", "ARZA") {}
 
     function mintNFT(address recipient) public returns (uint256) {
         _currentTokenId += 1;
--- utils/deployed-address.ts
+++ utils/deployed-address.ts
@@ -1,3 +1,3 @@
-const deployedAddress = '0xaCFeA974A53068Af6311830b0493d9ede78eB1c8'
+const deployedAddress = '0x89100F1F07681A41EAF0022E56069958D40eD533'
 
 export default deployedAddress
